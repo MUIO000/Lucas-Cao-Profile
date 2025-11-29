21c089050d40e5ef2a96a325ce9e04c950183267
caoshizhao <1602336289@qq.com>
2025-11-29 23:53:11 +1100

    feat: update Vite configuration to set output directory to 'docs'; add new CNAME file and index.html; remove outdated DESIGN_SYSTEM.md; add various assets including images and videos

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // 使用相对路径，这样可以直接打开 HTML 文件
+  build: {
+    outDir: 'docs',
+  },
   server: {
     port: 3000,
     open: true
